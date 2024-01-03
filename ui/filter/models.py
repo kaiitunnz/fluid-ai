41a8e56d5ba5309e5beecb93750f99e351c7b07f
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-01-04 04:58:22 +0900

    Fix bugs and add documentation

--- ui/filter/models.py
+++ ui/filter/models.py
@@ -11,11 +11,49 @@ from ...torchutils import BatchLoader, load_model
 
 
 class BaseUiFilter(UiDetectionModule):
+    """
+    A base class for a UI filter, aka invalid UI detection model.
+
+    A class that implements a UI filter to be used in the UI detection pipeline must
+    inherit this class.
+    """
+
     @abstractmethod
     def filter(self, elements: List[UiElement]) -> List[UiElement]:
+        """Filters out invalid UI elements
+
+        Caller should consider using the `__call__` method instead.
+
+        Parameters
+        ----------
+        elements : List[UiElement]
+            List of `UiElement`s to be filtered.
+
+        Returns
+        -------
+        List[UiElement]
+            List of `UiElement`s with invalid UI elements filtered out.
+        """
         raise NotImplementedError()
 
     def prefilter(self, elements: List[UiElement]) -> List[UiElement]:
+        """Filters out invalid UI elements based on heuristics
+
+        It can be used to preprocess the input list of UI elements to ensure that
+        the actual filtering algorithm works properly. The default implementation
+        is to filter out elements with invalid bounding boxes.
+
+        Parameters
+        ----------
+        elements : List[UiElement]
+            List of `UiElement`s to be preprocessed.
+
+        Returns
+        -------
+        List[UiElement]
+            List of preprocessed `UiElement`s with some `UIElement`s filtered out
+            based on a set of rules.
+        """
         # Check bounding boxes
         elements = [
             element for element in elements if self.__class__.is_valid_bbox(element)
@@ -24,25 +62,80 @@ class BaseUiFilter(UiDetectionModule):
         return elements
 
     def __call__(self, elements: List[UiElement]) -> List[UiElement]:
+        """Filters out invalid UI elements
+
+        It calls `self.prefilter` followed by `self.filter`.
+
+        Parameters
+        ----------
+        elements : List[UiElement]
+            List of `UiElement`s to be filtered.
+
+        Returns
+        -------
+        List[UiElement]
+            List of `UiElement`s with invalid UI elements filtered out.
+        """
         return self.filter(self.prefilter(elements))
 
     @staticmethod
     def is_valid_bbox(element: UiElement) -> bool:
+        """Tests if a `UiElement`'s bounding box is valid
+
+        Parameters
+        ----------
+        element : UiElement
+            A `UiElement` to be tested.
+
+        Returns
+        -------
+        bool
+            Wheter a `UiElement` is valid.
+        """
         w, h = array_get_size(element.get_cropped_image())
         return w > 0 and h > 0 and element.bbox.is_valid()
 
     @staticmethod
     def preprocess_image(image: Array) -> torch.Tensor:
-        tensor_image = torch.unsqueeze(array_to_tensor(image), 0)
+        """Preprocesses an image for model's inference
+
+        Parameters
+        ----------
+        image : Array
+            An image to be preprocessed. Must conform to the Array format.
+
+        Returns
+        -------
+        Tensor
+            The resulting PyTorch Tensor.
+        """
+        tensor_image = array_to_tensor(image)
         return tensor_image
 
 
 class DummyUiFilter(BaseUiFilter):
+    """
+    A dummy UI filter.
+
+    Simply returns the input.
+    """
+
     def filter(self, elements: List[UiElement]) -> List[UiElement]:
         return elements
 
 
 class TorchUiFilter(BaseUiFilter):
+    """
+    A base class of UI filters based on PyTorch models.
+
+    Attributes
+    ----------
+    model : FilterModelWrapper
+        A UI filter model wrapped in a `FilterModelWrapper`.
+    batch_size : int
+        Batch size for batched inference.
+    """
+
     model: FilterModelWrapper
     batch_size: int
 
@@ -52,6 +145,17 @@ class TorchUiFilter(BaseUiFilter):
         batch_size: int = 1,
         device: torch.device = torch.device("cpu"),
     ):
+        """
+        Parameters
+        ----------
+        model_path : str
+            Path to the model checkpoint. It should have been saved by
+            `fluid_ai.torchutils.save_model`.
+        batch_size : int
+            Batch size for batched inference.
+        device : device
+            The device on which the model will run.
+        """
         super().__init__()
         model = load_model(model_path).to(device)
         assert isinstance(model, FilterModelWrapper)
@@ -86,14 +190,43 @@ class TorchUiFilter(BaseUiFilter):
 
     @abstractmethod
     def transform(self, element: UiElement) -> torch.Tensor:
+        """Transforms a `UiElement` into a Tensor, which will be input to the
+        PyTorch model.
+
+        Parameters
+        ----------
+        element : UiElement
+            A UI element to be transformed.
+
+        Returns
+        -------
+        Tensor
+            The resulting Tensor.
+        """
         raise NotImplementedError()
 
     @abstractmethod
     def transform_batch(self, elements: List[UiElement]) -> List[torch.Tensor]:
+        """Transforms `UiElement`s in a list, which will be input to the PyTorch model.
+
+        Parameters
+        ----------
+        elements : List[UiElement]
+            List of `UiElement`s to be transformed.
+
+        Returns
+        -------
+        List[Tensor]
+            List of resulting Tensors.
+        """
         raise NotImplementedError()
 
 
 class ElementUiFilter(TorchUiFilter):
+    """
+    An element-based UI filter.
+    """
+
     def transform(self, element: UiElement) -> torch.Tensor:
         return self.model.transform(
             self.__class__.preprocess_image(element.get_cropped_image())
@@ -104,6 +237,10 @@ class ElementUiFilter(TorchUiFilter):
 
 
 class BoundaryUiFilter(TorchUiFilter):
+    """
+    A boundary-based UI filter.
+    """
+
     def transform(self, element: UiElement) -> torch.Tensor:
         transformed = self.model.transform(
             self.__class__.preprocess_image(element.get_screenshot())
