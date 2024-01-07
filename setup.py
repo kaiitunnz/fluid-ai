02fc1982b2d9c4378507b9a2a423cf8905f671b8
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-01-07 21:16:23 +0900

    Move to src

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from src.fluid_ai import __version__
+
+setup(
+    name="fluid_ai",
+    version=__version__,
+    url="https://github.com/kaiitunnz/fluid-ai",
+    author="Noppanat Wadlom",
+    author_email="noppanat.wad@gmail.com",
+    py_modules=["fluid_ai"],
+)
