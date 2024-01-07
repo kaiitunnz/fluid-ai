ebbe9469464f5bcfad601a67fe51559d4012b859
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-01-07 21:35:01 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 from src.fluid_ai import __version__
 
+with open("requirements.txt", "r") as f:
+    install_requires = [line for line in f if not line.lstrip().startswith("#")]
+
 setup(
     name="fluid_ai",
     version=__version__,
     url="https://github.com/kaiitunnz/fluid-ai",
     author="Noppanat Wadlom",
     author_email="noppanat.wad@gmail.com",
-    py_modules=["fluid_ai"],
+    packages=["fluid_ai"],
+    package_dir={"": "src"},
+    install_requires=install_requires,
 )
